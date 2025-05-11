a4394ec07eda4d4c878d4ab90f33fc3f83220134
Trivendra_Singh <trivendrasingh0711@gmail.com>
2025-05-11 16:48:03 +0530

    added project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,16 +2,50 @@ import mealapp from "../assests/meal_app.png"
 import contactform from "../assests/images.jpg"
 import donor from "../assests/donor2.png"
 import ProjectCard from './ProjectCard';
-import fpo from "../assests/download.png"
+// import fpo from "../assests/download.png"
 import Brainly from "../assests/Brainly.png"
 import ChatAppImage from "../assests/chatapp.png"
 import CareerCoachImage from "../assests/CareerCoachImage.png"
 import Finace_app from "../assests/home.jpg"
+import PeopleBloodApp from "../assests/PeopleBloodApp.jpg"
+import SMG_app from "../assests/SMG_app.jpg"
+import YathaavatSite from "../assests/YathaavatSite.jpg"
+
 
 export default function Projects() {
   const projects = [
-    
+
+  {
+  title: "People Blood Donation App",
+  description: "A full-stack blood donation management system that connects donors and recipients seamlessly. The app features real-time updates, secure authentication, and efficient data management. Built using React.js and TypeScript for the frontend, Node.js and Express for the backend, with MongoDB for persistent storage. It includes toast notifications, data filtering, and dynamic UI with Tailwind CSS.",
+  tech: ["React.js", "TypeScript", "Tailwind CSS", "Node.js", "Express", "MongoDB"],
+  github: "https://github.com/singhtrivendra/people_blood_fe",
+  live: "https://people-blood-fe.vercel.app",
+  image: PeopleBloodApp, 
+  isLive: true,
+},
+
     {
+  title: "SMG Electric",
+  description: "A modern EV solutions platform that helps users locate the nearest charging stations using Dijkstra's algorithm integrated with Google Maps API. Built with React.js, Node.js, and TypeScript for a robust full-stack solution. The project features clean routing, a visually appealing UI with Tailwind CSS, and MongoDB for scalable data handling.",
+  tech: ["React.js", "Node.js", "TypeScript", "Tailwind CSS", "MongoDB", "Google Maps API", "Dijkstra Algorithm"],
+  github: "https://github.com/singhtrivendra/smg_dev",
+  live: "https://smg-dev.vercel.app/",
+  image: SMG_app, 
+  isLive: true,
+}
+,
+{
+  title: "Yathaavat Official Website",
+  description: "A fully customized business website developed from scratch for Yathaavat Sol Pvt Ltd, featuring product listings, contact forms, quote requests, and a secure admin panel for seamless business operations. Built with a full-stack architecture and deployed using a VPS on Hostinger. The project includes backend setup, server configuration, and domain integration, offering a reliable and scalable solution for managing company outreach and operations.",
+  tech: ["React.js", "Node.js", "Express", "MongoDB", "Tailwind CSS", "VPS (Hostinger)", "REST API"],
+  github: "", 
+  live: "https://yathaavat.com/",
+  image: YathaavatSite, // Replace with the actual image import
+  isLive: true,
+}
+,
+{
       title: "Finance Tracker",
       description: "A comprehensive finance tracker app that helps users efficiently manage their expenses and income. Built with Flutter for a seamless UI experience, Firebase for cloud-based services, and SQLite/Hive for offline data storage. The app supports secure authentication, real-time synchronization, and state management for optimized performance.",
       tech: ["Flutter", "Firebase", "FCM", "SQLite", "Hive", "Provider"],
@@ -68,14 +102,14 @@ export default function Projects() {
       isLive:false,
       image: "https://images.unsplash.com/photo-1551650975-87deedd944c3?auto=format&fit=crop&q=80&w=1974",
     },
-    {
-      title: "contact Form App",
-      description: "Android application for picking image and location",
-      tech: ["Flutter", "Android", "Geolocation","image_picker"],
-      github: "https://github.com/singhtrivendra/contact_form_app",
-      isLive:false,
-      image: contactform,
-    },
+    // {
+    //   title: "contact Form App",
+    //   description: "Android application for picking image and location",
+    //   tech: ["Flutter", "Android", "Geolocation","image_picker"],
+    //   github: "https://github.com/singhtrivendra/contact_form_app",
+    //   isLive:false,
+    //   image: contactform,
+    // },
     {
       title: "Meal App",
       description: "Android application for meal recipes and filters",
@@ -84,14 +118,14 @@ export default function Projects() {
       isLive:false,
       image: mealapp,
     },
-    {
-      title: "Blood Donor App",
-      description: "DonorConnect, an application designed to connect donors and organizations for life-saving contributions.",
-      tech: ["Flutter", "Android", "Firebase"],
-      github: "https://github.com/singhtrivendra/donorconnect",
-      isLive:false,
-      image: donor,
-    },
+    // {
+    //   title: "Blood Donor App",
+    //   description: "DonorConnect, an application designed to connect donors and organizations for life-saving contributions.",
+    //   tech: ["Flutter", "Android", "Firebase"],
+    //   github: "https://github.com/singhtrivendra/donorconnect",
+    //   isLive:false,
+    //   image: donor,
+    // },
     // {
     //   title: "FPO for farmers and traders",
     //   description: "Web  UI serves as a valuable resource for farmers seeking to join FPOs ",
